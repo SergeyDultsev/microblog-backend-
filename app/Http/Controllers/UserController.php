2b5087c1cd09f109b0a50a875543d6eea5373166
SergeyDultsev <121752321+Dultsev2004@users.noreply.github.com>
2024-06-07 18:32:22 +0700

    add users methods

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,5 +7,111 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function getUser($userId)
+    {
+        $user = User::find($userId);
+
+        if (!$user) {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+
+        return response()->json($user);
+    }
+
+    public function updateRole(Request $request, User $userId)
+    {
+        // Имеет ли текущий пользователь право изменять роль
+        if (!$request->user()->isAdmin()) {
+            return response()->json(['error' => 'Unauthorized'], 403);
+        }
+
+        $user = User::find($userId->id);
+
+        // Существует ли пользователь
+        if (!$user) {
+            return response()->json(['error' => 'User not found'], 404);
+        }
+
+        // Наличие параметра роли
+        if (!$request->has('role')) {
+            return response()->json(['error' => 'Role parameter is required'], 400);
+        }
+
+        // Переданное значение роли допустимо (0 или 1)
+        $role = $request->input('role');
+        if (!in_array($role, [0, 1])) {
+            return response()->json(['error' => 'Invalid role value'], 400);
+        }
+
+        $user->role = $role;
+        $user->save();
+
+        return response()->json(['message' => 'User role updated successfully']);
+    }
+
+    public function updateUserAbout(Request $request, User $userId)
+    {
+        $user = User::find($userId);
+
+        // Наличие параметра роли
+        if (!$request->has('about')) {
+            return response()->json(['error' => 'Role parameter is required'], 400);
+        }
+
+        $user->save();
+    }
+
+    public function updateUserAvatar(Request $request, User $userId)
+    {
+        $user = User::find($userId->id);
+
+        // Наличие параметра роли
+        if (!$request->has('profile_avatar_url')) {
+            return response()->json(['error' => 'Role parameter is required'], 400);
+        }
+    }
+
+    public function updateUserHeadAvatar(Request $request, User $userId)
+    {
+        $user = User::find($userId);
+
+        // Наличие параметра роли
+        if (!$request->has('profile_head_avatar_url')) {
+            return response()->json(['error' => 'Role parameter is required'], 400);
+        }
+    }
+
+    public function deleteUser($userId)
+    {
+        $user = User::find($userId);
+
+        if ($user) {
+            $user->delete();
+            Auth::logout();
+            return response()->json(['message' => 'User deleted successfully']);
+        } else {
+            return response()->json(['error' => 'User not found'], 404);
+        }
+    }
+
+    public function searchUser(Request $request)
+    {
+        $query = $request->query('query');
+
+
+        if (!$query) {
+            return response()->json(['error' => 'Параметр запроса обязателен'], 400);
+        }
+
+        // Фильтруем пользователей по имени и фамилии
+        $users = User::where('name', 'LIKE', "%{$query}%")
+            ->orWhere('surname', 'LIKE', "%{$query}%")
+            ->get();
+
+        if ($users->isEmpty()) {
+            return response()->json(['message' => 'Пользователи не найдены'], 404);
+        }
+
+        return response()->json($users);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -8,8 +9,19 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+// Публичные маршруты
+Route::get('/users/{userId}', [UserController::class, 'getUser']);
+Route::get('/users/search', [UserController::class, 'searchUser']);
+
 // Защищенные маршруты
-Route::middleware(['auth:sanctum'])->group(function () {
+Route::middleware(['auth:sanctum'])->group(function() {
     // Маршрут для выхода
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    // Маршруты для пользователей
+    Route::put('/users/{userId}/role', [UserController::class, 'updateRole'])->middleware('admin');
+    Route::patch('/users/{userId}/about', [UserController::class, 'updateUserAbout']);
+    Route::patch('/users/{userId}/avatar', [UserController::class, 'updateUserAvatar']);
+    Route::patch('/users/{userId}/head-avatar', [UserController::class, 'updateUserHeadAvatar']);
+    Route::delete('/users/{userId}', [UserController::class, 'deleteUser']);
 });
